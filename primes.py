a99032a83706b3af17041d3a508f4c2b8169d768
Shrestha, Saujan (k21075139) <saujan.shrestha@kcl.ac.uk>
2022-09-27 19:34:54 +0100

    Add functionality and exception handling to the primes function

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
+from math import sqrt, floor
 
 def primes(number_of_primes):
-    list = []
+    if (number_of_primes <= 0):
+        raise ValueError("Number of primes must not be lower than 1.")
+
+    list = [2]
+    i = 1
+    count = 1
+
+    while count < number_of_primes:
+        i += 2
+        for k in range(2, 1+floor(sqrt(i+1))):
+            if i%k == 0:
+                break
+        else:
+            list.append(i)
+            count += 1
+
     return list
+
+print(primes(100))
